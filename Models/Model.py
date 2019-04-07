cc15ede72f18f5d76943180001f6b40bec7bf184
miaozeyu <miaozeyu@gmail.com>
2019-04-07 19:16:29 -0400

    Initial commit includes Flask app main function, Posting(Model) and data_provider_service which includes functions to initialize DB, add posting and get posting(s)

---
+++ Models/Model.py
@@ -0,0 +1,4 @@
+#this is the declarative_base model
+from sqlalchemy.ext.declarative import declarative_base
+
+Model = declarative_base()
---
+++ Models/Posting.py
@@ -0,0 +1,38 @@
+
+# id
+# Date
+# Job_Title
+# City
+# Technologies
+# Job_Type
+# Company
+# Links
+
+from Model import Model
+from sqlalchemy import Column, String, Date, Integer
+
+class Posting(Model):
+    __tablename__ = 'postings'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(Date, nullable=False)
+    job_title = Column(String(100), nullable=False)
+    company = Column(String(50), nullable=False)
+    city = Column(String(50), nullable=True)
+    technologies = Column(String(1000), nullable=True)
+    job_type = Column(String(20), nullable=True)
+    links = Column(String(200), nullable=True)
+
+
+#
+# Methods
+#
+    def serialize(self):
+        return {
+            "date": self.date,
+            "job_title": self.job_title,
+            "company": self.company,
+            "city": self.city,
+            "technologies": self.technologies,
+            "job_type": self.job_type,
+            "links": self.links
+        }
